9a7ad62548a8d3c6330ded2e3dc62a57c28804d5
Deepak Vishwakarma <vishwakarmadeepak310@gmail.com>
2026-08-29 06:39:06 +0530

    feat(port): define input pool repository

---
+++ lib/agri_relay/ports/input_pool_repository.ex
@@ -0,0 +1,6 @@
+defmodule AgriRelay.Ports.InputPoolRepository do
+  @moduledoc "Persistence port for input pool repository."
+  @callback get(cooperative_id :: term(), id :: term()) :: {:ok, map()} | {:error, term()}
+  @callback save(cooperative_id :: term(), record :: map()) :: {:ok, map()} | {:error, term()}
+  @callback list(cooperative_id :: term(), opts :: keyword()) :: {:ok, [map()]} | {:error, term()}
+end
