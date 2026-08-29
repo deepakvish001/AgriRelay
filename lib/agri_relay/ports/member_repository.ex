22013b31b06039affa8c027ec68cfd8ddd9c760f
Deepak Vishwakarma <vishwakarmadeepak310@gmail.com>
2026-08-29 06:38:32 +0530

    feat(port): define member repository

---
+++ lib/agri_relay/ports/member_repository.ex
@@ -0,0 +1,6 @@
+defmodule AgriRelay.Ports.MemberRepository do
+  @moduledoc "Persistence port for member repository."
+  @callback get(cooperative_id :: term(), id :: term()) :: {:ok, map()} | {:error, term()}
+  @callback save(cooperative_id :: term(), record :: map()) :: {:ok, map()} | {:error, term()}
+  @callback list(cooperative_id :: term(), opts :: keyword()) :: {:ok, [map()]} | {:error, term()}
+end
